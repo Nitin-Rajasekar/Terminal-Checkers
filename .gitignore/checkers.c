5d8adfccbd507836192e26aef76c7b7beceab172
Rohannain <79190972+Rohannain@users.noreply.github.com>
2021-04-26 12:27:40 +0530

    Update checkers.c

--- .gitignore/checkers.c
+++ .gitignore/checkers.c
@@ -107,8 +107,86 @@ void Logic(int player, int x1, int x2, int y1, int y2, int arr[10][10]){        
         else /*code for invalid input*/;
     }
     else if(player==2){
-        /*Someone please fill this up lol, I'm too lazy.
-        Just need to copy paste the code for player 1, and
+        
+   if(arr[x1][y1]==1){                                                         //non-crowned
+            if((y2-y1==1)&&((x2-x1==1)||(x1-x2==1))){                               //not a jump
+                if(arr[x2][y2]==0){
+                    arr[x1][y1]=0;
+                    arr[x2][y2]=1;
+                }
+                else /*code for invalid input*/;
+            }
+            else if(y2-y1==2){                                                      //jump
+                if(x2-x1==2){                                                       //right jump
+                    if((arr[x1+1][y1+1]==2||arr[x1+1][y1+1]==4)&&(arr[x2][y2]==0)){
+                        arr[x1+1][y1+1]=0;                                          //removing the opponent's pawn
+                        arr[x2][y2]=1;
+                        arr[x1][y1]=0;
+                    }
+                    else /*code for invalid input*/;
+                }
+                else if (x1-x2==2){                                                 //left jump
+                    if((arr[x1-1][y1+1]==2||arr[x1-1][y1+1]==4)&&(arr[x2][y2]==0)){
+                        arr[x1-1][y1+1]=0;                                          //removing the opponent's pawn
+                        arr[x2][y2]=1;
+                        arr[x1][y1]=0;
+                    }
+                    else /*code for invalid input*/;
+                }
+                else /*code for invalid input*/;
+            }
+            else /*code for invalid input*/;
+        }
+        else if(arr[x1][y1]==3){                                                    //crowned
+            if(((y2-y1==1)||(y1-y2==1))&&((x2-x1==1)||(x1-x2==1))){                 //non-jump                         
+                if(arr[x2][y2]==0){
+                    arr[x1][y1]=0;
+                    arr[x2][y2]=3;
+                }
+                else /*code for invalid input*/;
+            }
+            else if(y2-y1==2){                                                      //forward jump                                         
+                if(x2-x1==2){                                                      
+                    if((arr[x1+1][y1+1]==2||arr[x1+1][y1+1]==4)&&(arr[x2][y2]==0)){
+                        arr[x1+1][y1+1]=0;                                          
+                        arr[x2][y2]=3;
+                        arr[x1][y1]=0;
+                    }
+                    else /*code for invalid input*/;
+                }
+                else if (x1-x2==2){                                                 
+                    if((arr[x1-1][y1+1]==2||arr[x1-1][y1+1]==4)&&(arr[x2][y2]==0)){
+                        arr[x1-1][y1+1]=0;                                          
+                        arr[x2][y2]=3;
+                        arr[x1][y1]=0;
+                    }
+                    else /*code for invalid input*/;
+                }
+                else /*code for invalid input*/;
+            }
+            else if(y1-y2==2){                                                      //backward jump                                          
+                if(x2-x1==2){                                                      
+                    if((arr[x1+1][y2+1]==2||arr[x1+1][y2+1]==4)&&(arr[x2][y2]==0)){
+                        arr[x1+1][y2+1]=0;                                          
+                        arr[x2][y2]=3;
+                        arr[x1][y1]=0;
+                    }
+                    else /*code for invalid input*/;
+                }
+                else if (x1-x2==2){                                                 
+                    if((arr[x1-1][y2+1]==2||arr[x1-1][y2+1]==4)&&(arr[x2][y2]==0)){
+                        arr[x1-1][y2+1]=0;                                          
+                        arr[x2][y2]=3;
+                        arr[x1][y1]=0;
+                    }
+                    else /*code for invalid input*/;
+                }
+                else /*code for invalid input*/;
+            }
+            else /*code for invalid input*/;
+        }
+        else /*code for invalid input*/;
+    }   /*Someone please fill this up lol, I'm too lazy.  Just need to copy paste the code for player 1, and
         adjust for the fact that player 2's pawns are labelled
         2,4 and move downward by default, not upwards.*/
     }
